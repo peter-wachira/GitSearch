6e6a8701631189c96f3682a36ebcd688bab1c4c9
pwachira900@gmail.com <pwachira900@gmail.com>
2019-04-01 10:10:51 +0300

    added scroll top button

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,12 +2,13 @@
 // tslint:disable-next-line:import-blacklist
 import { Observable} from 'rxjs/Rx';
 import { repos} from '../repos';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, HostListener } from '@angular/core';
 import { GithubService } from '../github.service';
 import {environment} from '../../environments/environment';
 import {ProfileRequestService} from '../profile-request.service';
 import { user } from '../user';
 import { SweetAlertService } from 'angular-sweetalert-service';
+import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-dashboard',
@@ -22,19 +23,42 @@ export class DashboardComponent implements OnInit {
 
   loading = false;
   errorMessage;
+  windowScrolled: boolean;
   // tslint:disable-next-line:max-line-length
-  constructor( private githubService: GithubService, private profileRequest: ProfileRequestService, private alertService: SweetAlertService ) {
+  constructor( private githubService: GithubService, private profileRequest: ProfileRequestService, private alertService: SweetAlertService, @Inject(DOCUMENT) private document: Document ) {
   }
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+      if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
+          this.windowScrolled = true;
+      } else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
+          this.windowScrolled = false;
+      }
+  }
+  scrollToTop() {
+      (function smoothscroll() {
+          const currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+          if (currentScroll > 0) {
+              window.requestAnimationFrame(smoothscroll);
+              window.scrollTo(0, currentScroll - (currentScroll / 8));
+            }
+          })();
+      }
 
-    options = {
-    title: 'Are you sure?',
-    text: 'You won\'t be able to revert this!',
-    type: 'warning',
-    showCancelButton: true,
-    confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
-    confirmButtonText: 'Yes, delete it!'
-  };
+
+  //   options = {
+  //   title: 'Are you sure?',
+  //   text: 'You won\'t be able to revert this!',
+  //   type: 'warning',
+  //   showCancelButton: true,
+  //   confirmButtonColor: '#3085d6',
+  //   cancelButtonColor: '#d33',
+  //   confirmButtonText: 'Yes, delete it!'
+  // };
+
+
+
+
 public getRepos(event: any) {
   this.loading = true;
   // tslint:disable-next-line:prefer-const
